3f6fcfb8f560278eba4eb1cae81e046480c0c890
Martin <IchHeibemartus@gmail.com>
2020-07-28 16:30:47 -0300

    commitei o código inteiro

--- semana3/operadores/js-vanilla-template (2)/js-vanilla-template/src/index.js
+++ semana3/operadores/js-vanilla-template (2)/js-vanilla-template/src/index.js
@@ -1,161 +1,161 @@
-//EXERCÍCIOS DE INTERPRETAÇÃO
+// //EXERCÍCIOS DE INTERPRETAÇÃO
 
-//1. 
+// //1. 
 
-// a.false
-// b.false
-// c.false
-// e.false
-
-
-//2.
-
-// a.undefined
-// b.null
-// c.10
-// d.[0]
-// e.[19]
-// f.[25]
+// // a.false
+// // b.false
+// // c.false
+// // e.false
 
 
-//EXERCÍCIOS DE ESCRITA 
+// //2.
 
-//1 
-
-//a
-const idade = prompt("Qual a sua idade?")
-//b
-const idadeMelhorAmigo = prompt("Qual a idade do seu melhor amigo?")
-
-const age = Number(idade)
-const ageMelhorAmigo = Number(idadeMelhorAmigo)
-
-//c
-const resposta = idade > idadeMelhorAmigo
-
-console.log("Sua idade é maior do que dos seu amigo? " + resposta )
-
-//d
-const diferenca = idade - idadeMelhorAmigo
-console.log(diferenca)
+// // a.undefined
+// // b.null
+// // c.10
+// // d.[0]
+// // e.[19]
+// // f.[25]
 
 
-//2
+// //EXERCÍCIOS DE ESCRITA 
 
-//a
-const par = prompt("Escreva um número par")
-Number(par)
+// //1 
 
-//b
-console.log(par % 2)
-
-//c. O resultando sempre é 0 não importa o número par usado.
-
-//d. Se o usuário usar um número ímpar o resultado sempre será 1.
-
-
-//3
-
-//a
- let listaDeTarefas = []
-
+// //a
+// const idade = prompt("Qual a sua idade?")
 // //b
- const item1 = prompt("Diga")
- const item2 = prompt("tarefa 2")
- const item3 = prompt("tarefa3")
+// const idadeMelhorAmigo = prompt("Qual a idade do seu melhor amigo?")
 
- listaDeTarefas.push(item1,item2,item3) 
+// const age = Number(idade)
+// const ageMelhorAmigo = Number(idadeMelhorAmigo)
 
 // //c
- console.log( listaDeTarefas);
+// const resposta = idade > idadeMelhorAmigo
 
-//d
-let tarefa = prompt("Digite o índice da tarefa completada: 0, 1, 2")
+// console.log("Sua idade é maior do que dos seu amigo? " + resposta )
 
-//e
-listaDeTarefas.splice(tarefa, 1)
-
-//f
-console.log(listaDeTarefas)
+// //d
+// const diferenca = idade - idadeMelhorAmigo
+// console.log(diferenca)
 
 
-//4
+// //2
 
-const usuario = prompt ("Qual seu nome de usuário?")
+// //a
+// const par = prompt("Escreva um número par")
+// Number(par)
 
-const email = prompt("Qual seu endereço de e-mail atualizado?")
+// //b
+// console.log(par % 2)
 
-const cadastro = ("O e-mail " + email + " foi cadastrado com sucesso. Seja bem-vinda(o), " + usuario+"!")
+// //c. O resultando sempre é 0 não importa o número par usado.
 
-console.log(cadastro )
-
-
-
-//DESAFIO
-
-//1
-
-//a
-let equa = (kelvin) = (77 - 32) * 5 / 9 + 273.15
-console.log(equa, "KELVIN")
-
-//b
-let equa1 = (GRAUS_FAHRENHEIT) = (80) * 9 / 5 + 32
-console.log(equa1,"GRAUS_FAHRENHEIT" )
-
-//c
-let equa2 = (GRAUS_FAHRENHEIT) = (30) * 9 / 5 + 32
-let equa3 = (Kelvin) = (30) + 273
-console.log(equa2, "C°,", equa3, "K")
-
-//d
-let equa4 = prompt("Digite o valor em graus celsius que desejaconverter")
-equa3 = (KELVIN) = Number(equa4) + 273
-console.log(equa3)
+// //d. Se o usuário usar um número ímpar o resultado sempre será 1.
 
 
-//2
+// //3
 
-//a
-let Qw = prompt("Coloque a o valor de Quilowatt-hora consumido")
-let total = (Qw) * 0.05
-//console.log("R$" + consumo)
+// //a
+//  let listaDeTarefas = []
 
-//b
-let desconto = prompt("Insira o desconto" , "%")
-total = (Qw) * 0.05 - desconto/100
-console.log("R$" + total)
+// // //b
+//  const item1 = prompt("Diga")
+//  const item2 = prompt("tarefa 2")
+//  const item3 = prompt("tarefa3")
+
+//  listaDeTarefas.push(item1,item2,item3) 
+
+// // //c
+//  console.log( listaDeTarefas);
+
+// //d
+// let tarefa = prompt("Digite o índice da tarefa completada: 0, 1, 2")
+
+// //e
+// listaDeTarefas.splice(tarefa, 1)
+
+// //f
+// console.log(listaDeTarefas)
 
 
-//3
+// //4
 
-//a
-let a = (20) / (2.205)
-console.log(a)
+// const usuario = prompt ("Qual seu nome de usuário?")
 
-//b
-let b = (10.5) / (35.274)
-console.log(b)
+// const email = prompt("Qual seu endereço de e-mail atualizado?")
 
-//c
-let c = (100) * (1609)
-console.log(c)
+// const cadastro = ("O e-mail " + email + " foi cadastrado com sucesso. Seja bem-vinda(o), " + usuario+"!")
 
-//d
-let d = (50) / (3.281)
-console.log(d)
+// console.log(cadastro )
 
-//e
-let e = (103.56) * (3.785)
-console.log(e)
 
-//f
-let f = (450) / (3.52)
-console.log(f)
 
-//g
-let g = (450) / (f)
-let h = prompt("Dígite um valor em xic para converter em li")
-let i = (h) / (g)
+// //DESAFIO
 
-console.log(i)
+// //1
+
+// //a
+// let equa = (kelvin) = (77 - 32) * 5 / 9 + 273.15
+// console.log(equa, "KELVIN")
+
+// //b
+// let equa1 = (GRAUS_FAHRENHEIT) = (80) * 9 / 5 + 32
+// console.log(equa1,"GRAUS_FAHRENHEIT" )
+
+// //c
+// let equa2 = (GRAUS_FAHRENHEIT) = (30) * 9 / 5 + 32
+// let equa3 = (Kelvin) = (30) + 273
+// console.log(equa2, "C°,", equa3, "K")
+
+// //d
+// let equa4 = prompt("Digite o valor em graus celsius que desejaconverter")
+// equa3 = (KELVIN) = Number(equa4) + 273
+// console.log(equa3)
+
+
+// //2
+
+// //a
+// let Qw = prompt("Coloque a o valor de Quilowatt-hora consumido")
+// let total = (Qw) * 0.05
+// //console.log("R$" + consumo)
+
+// //b
+// let desconto = prompt("Insira o desconto" , "%")
+// total = (Qw) * 0.05 - desconto/100
+// console.log("R$" + total)
+
+
+// //3
+
+// //a
+// let a = (20) / (2.205)
+// console.log(a)
+
+// //b
+// let b = (10.5) / (35.274)
+// console.log(b)
+
+// //c
+// let c = (100) * (1609)
+// console.log(c)
+
+// //d
+// let d = (50) / (3.281)
+// console.log(d)
+
+// //e
+// let e = (103.56) * (3.785)
+// console.log(e)
+
+// //f
+// let f = (450) / (3.52)
+// console.log(f)
+
+// //g
+// let g = (450) / (f)
+// let h = prompt("Dígite um valor em xic para converter em li")
+// let i = (h) / (g)
+
+// console.log(i)
