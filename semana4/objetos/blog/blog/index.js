12d21e3beb8f5d3b1519270bb5c5da37ce128a9a
Martin <IchHeibemartus@gmail.com>
2020-08-05 17:52:26 -0300

    Adicionei comentários e removi códigos que não tinha utilidade

--- semana4/objetos/blog/blog/index.js
+++ semana4/objetos/blog/blog/index.js
@@ -1,44 +1,25 @@
-// const titulo = document.getElementById('titulo-post')
-// const autor = document.getElementById('autor-post')
-// const conteudo = document.getElementById('conteudo-post')
-
-// const formulario = {
-//     titulo: document.getElementById('titulo-post'),
-//     autor: document.getElementById('autor-post'),
-//     conteudo: document.getElementById('conteudo-post')
-// }
-
-// // console.log(formulario)
-// function adicionaPost() {
-//     const container = document.getElementById('container-de-posts')
-//     container.innerHTML += `<p>${formulario.titulo.value}</p> <p>${formulario.autor.value}</p> <p>${formulario.conteudo.value}</p>`
-//     formulario.titulo.value = " "
-//     formulario.autor.value = " "
-//     formulario.conteudo.value = " "
-// }
-
-
-
 
 function adicionaPost() {
-    const container = document.getElementById('container-de-posts').value
-    const titulo = document.getElementById('titulo-post').value
+    const container = document.getElementById('container-de-posts')
+    const titulo = document.getElementById('titulo-post')
     const autor = document.getElementById('autor-post')
     const conteudo = document.getElementById('conteudo-post')
-    container.innerHTML += `<div><p>${titulo.value}</p> <p>${autor.value}</p></div> <p>${conteudo.value}</p>`
     // Recebi as tags do HTML
+    container.innerHTML += `<div><p>${titulo.value}</p> <p>${autor.value}</p></div> <p>${conteudo.value}</p>`
+    // Adiciona os valores digitados no HTML
+
 
     const formulario = {
-        titulo: titulo,
+        titulo: titulo.value,
         autor: autor.value,
         conteudo: conteudo.value
     }// Agora ele adiciona os valores gerados na página
 
     const memoria = [] //Criei a array
-    memoria.push(formulario)// Adiciona os valores do objeto dentro do array
+    memoria.push(formulario) // Adiciona os valores do objeto dentro do array
 
-    console.log(formulario)// Imprime uma alteração por vez
-    console.log(memoria)// Imprime todas as alterações dentro do array
+    console.log(formulario) // Imprime uma alteração por vez
+    console.log(memoria) // Imprime todas as alterações dentro do array
 
     titulo.value = " "
     autor.value = " "
@@ -48,37 +29,39 @@ function adicionaPost() {
 
 // function redirect() {
 //     document.getElementById("de-posts").innerHTML = window.location.search
-// }
+// } //Era pra redirecionar a página
 
 function apertouEnter(event) {
     if (event.key === 'Enter') {
         adicionaPost()
     }
-}
+} // Essa função recebe a função principal e usa através do input
 
 
 function apertouBotao(event) {
     adicionaPost()
     //redirect()
+}// Essa função recebe a função principal e usa através do botão
+
+
+// Função adicionar imagem
+function identificarImagem() {
+    const imagem = document.getElementById('link-imagem').value
+    const container = document.getElementById('container-de-posts')
+    if (imagem.includes('.png') || imagem.includes('jpg')) {
+        container.innerHTML += `<img src=${imagem}>`
+        imagem.value = " "
+    } else {
+        alert('Link incorreto! Insira imagem válida')
+    }
+
 }
+function cliqueEnter(event) {
+    if (event.key === 'Enter') {
+        identificarImagem()
+    }
+}
+function cliqueImagem(event) {
+    identificarImagem()
 
-
-
-// function identificarImagem() {
-//     const imagem = document.getElementById('link-imagem').value
-//     const container = document.getElementById('container-de-posts')
-//     if (imagem.includes('.png') || imagem.includes('jpg')) {
-//         container.innerHTML += `<img src=${imagem}>`
-//     } else {
-//         alert('Link incorreto! Insira imagem válida')
-//     }
-// formulario.image = imagem.value
-// }
-// function cliqueEnter(event) {
-//     if (event.key === 'Enter') {
-//         identificarImagem()
-//     }
-// }
-// function cliqueImagem(event) {
-//     identificarImagem()
-// }
+}
