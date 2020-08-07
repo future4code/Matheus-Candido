1d309f13e6533ae836e3fa8e03699c1b2b6d198b
Martin <IchHeibemartus@gmail.com>
2020-08-07 15:28:55 -0300

    Exercicios de Logica concluido

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -56,3 +56,98 @@ Dois console log:
 Primeiro - imprime o array "numero1" que vai ter como valor o mesmo valor da array "numeros"
 Segundo - imprime o array "numero2" que vai ter como valor o mesmo valor da array "numeros" porém sem o -10 pois ele é menor que 0 e não passou na condição */
 
+
+// // EXERCÍCIOS DE LÓGICA DE PROGRAMAÇÃO // //
+
+//1
+// /*Usando For, while, arrayfunction */
+// const pokemons = [
+//     { nome: "Pikachu", tipo: "Elétrico" },
+//     { nome: "Charmeleon", Tipo: "Fogo" },
+//     { nome: "Pachirisu", tipo: "Elétrico" }
+// ]
+// //arrayfunction
+// const callback = (element, index, array) => {
+//     if (element.tipo === "Elétrico") {
+//        return element.tipo
+//     }
+
+// }
+// const poke = pokemons.filter(callback)
+// console.log(poke)
+
+
+//2
+//a - true
+//b - true
+//c - true
+//d - true
+//e - false
+
+
+//3
+/* Não funciona porque faltava complementos do loop, no caso faltava o i++ pra que o loop funcionasse corretamente, 
+e invez de dar resultado dentro do console log, faltou criar uma const result que fazia a operação e dar conseole log dessa const*/
+// const quantidadeDeNumerosPares =[1,2,3,4,5]
+// let i = 0
+// while (i < quantidadeDeNumerosPares.length) {
+//     const result = i * 2
+//     console.log(result)
+//     i++
+// }
+
+
+//4
+// function tringulos(a, b, c) {
+//     if (a === b && c === a) {
+//         console.log(`É um Equilátero`)
+//     }
+//     if (a === b && b !== c) {
+//         console.log(`É um Isóceles`)
+//     }
+//     if (a !== b && b === c) {
+//         console.log(`É um Isóceles`)
+//     }
+//     if (a === c && b !== c) {
+//         console.log(`É um Isóceles`)
+//     }
+//     if (a !== b && a !== c && b !== c) {
+//         console.log(`É um Escaleno`)
+//     }
+// }
+// const medidas = tringulos(10, 10, 10)
+
+
+//5
+// function numeros(numero1, numero2) {
+//     if (numero1 > numero2) {
+//         console.log(`O número é: ${numero1}`)
+//     }
+//     if (numero1 < numero2) {
+//         console.log(`O maior é: ${numero2}`)
+//     }
+//     if (numero1 % numero2 !== 0) {
+//         console.log(`${numero1} não é divisível por ${numero2}`)
+
+//         if (numero2 % numero1 === 0) {
+//             console.log(`${numero2} é divisível por ${numero1}`)
+//         }
+//     }
+//     if (numero2 % numero1 !== 0) {
+//         console.log(`${numero2} não é divisível por ${numero1}`)
+
+//         if (numero1 % numero1 === 0) {
+//             console.log(`${numero1} é divisível por ${numero2}`)
+//         }
+//     }
+//     if (numero1 > numero2) {
+//         const diff1 = numero1 - numero2
+//         console.log(`A diferença entre eles é ${diff1}`)
+//     }
+//     if (numero1 < numero2) {
+//         const diff2 = numero2 - numero1
+//         console.log(`A diferença entre eles é ${diff2}`)
+//     }
+// }
+// const resultado = numeros(30, 15)
+
