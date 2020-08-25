959853aedf22c51cb7f3b255c3ff15023c08e50e
Martin <IchHeibemartus@gmail.com>
2020-08-25 16:54:47 -0300

    ...

--- semana7/integracao-api/api-integration/src/App.js
+++ semana7/integracao-api/api-integration/src/App.js
@@ -6,18 +6,18 @@ import axios from "axios"
 export default class App extends React.Component {
   state = {
     name: "",
-    email: "",
+    // email: "",
     listUser: []
   }
 
   listarUsers = () => {
-    const body = {
-      // id: this.state.id,
-      name: this.state.nome
-    }
+    // const body = {
+    //   // id: this.state.id,
+    //   name: this.state.nome
+    // }
     const request = axios.get(
       "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-      body,
+      // body,
       {
         headers: {
           Authorization: "matheus-candido-jackson"
@@ -28,7 +28,7 @@ export default class App extends React.Component {
     request
       .then((resposta) => {
         console.log(resposta.data)
-        this.setState({ listUser: resposta.data.result.list })
+        this.setState({ listUser: resposta.data })
       })
       .catch((error) => {
         alert("lista errada")
@@ -36,60 +36,63 @@ export default class App extends React.Component {
   }
 
 
-  newUser = (name, email) => {
+  newUser = (nome) => {
     const body = {
-      name: name,
-      email: email
+      name: nome,
+      // email: email
     }
 
     const request = axios.post(
       "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body,
       {
         headers: {
+          "Content-Type": "application/json",
           Authorization: "matheus-candido-jackson"
         }
       }
     )
 
     request
-      .then((response) => {
-        console.log(response.data)
-        alert(`lalala ${this.listarUsers}`)
+      .then((resposta) => {
+        console.log(resposta.data)
+        // alert(`lalala ${this.listarUsers}`)
         this.listarUsers()
       })
       .catch((error) => {
-        alert("error lul!!")
+        console.log(error)
+        // alert("error lul!!")
       })
   }
 
   componentDidMount() {
     this.listarUsers()
   }
+
   onChangeName = (e) => {
     this.setState({ name: e.target.value })
   }
-  onChangeEmail = (e) => {
-    this.setState({ email: e.target.value })
+  // onChangeEmail = (e) => {
+  //   this.setState({ email: e.target.value })
 
-  }
+  // }
 
   render() {
     const addOnList = () => {
       this.newUser(this.state.name)
       this.setState({ name: "" })
-      this.newUser(this.state.email)
-      this.setState({ email: "" })
+      // this.newUser(this.state.email)
+      // this.setState({ email: "" })
     }
     return (
       <div className="App">
         <h1>insira seu usuario</h1>
-        <input value={this.state.name} onChange={(e) => this.onChangeName(e)} />
-        <input value={this.state.email} onChange={(e) => this.onChangeEmail(e)} />
+        <input value={this.state.name} onChange={(event) => this.onChangeName(event)} />
+        {/* <input value={this.state.email} onChange={(e) => this.onChangeEmail(e)} /> */}
         <button onClick={addOnList}>Criar usuario</button>
         <h1>lista usuarios</h1>
-        {this.state.listUser.map((item) => {
-          return <p key={item.id}>{item.name}</p>
-        })}
+        {this.state.listUser.map((item) => (
+          <p key={item.id}>{item.name}</p>
+        ))}
       </div>
     )
   }
