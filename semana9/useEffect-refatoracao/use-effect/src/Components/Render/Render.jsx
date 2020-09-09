377065d35faca683d502d25fb465a553338d077d
Martin <IchHeibemartus@gmail.com>
2020-09-09 15:08:33 -0300

    Tarefa Full

---
+++ semana9/useEffect-refatoracao/use-effect/src/Components/Render/Render.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import PokeCard from '../PokeCard/PokeCard'
+
+export default function Render(props) {
+    const [pokeList, setPokeList] = useState([])
+    const [pokeName, setPokeName] = useState()
+
+    const getPokemons = () => {
+        axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+            .then(r => {
+                setPokeList(r.data.results)
+            }).catch(e => {
+                console.log(e)
+            })
+    }
+
+    useEffect(() => {
+        getPokemons()
+    }, [])
+
+    const changePokeName = e => {
+        setPokeName(e.target.value)
+    }
+
+    return (
+        <div>
+            <select onChange={changePokeName}>
+                <option value="">Nenhum</option>
+                {pokeList.map(pokemon => {
+                    return (
+                        <option key={pokemon.name} value={pokemon.name}>
+                            {pokemon.name}
+                        </option>
+                    )
+                })}
+            </select>
+            {pokeName && <PokeCard pokemon={pokeName} />}
+        </div>
+    )
+}
