4b98675b158bff324ea1921cb5b05c6192c83f64
Martin <IchHeibemartus@gmail.com>
2020-09-15 16:51:48 -0300

    ...

--- semana10/labeX/labe-x/src/Components/Pages/ApplicationForm/ApplicationForm.jsx
+++ semana10/labeX/labe-x/src/Components/Pages/ApplicationForm/ApplicationForm.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function ApplicationForm() {
+    const goToback = () => {
+        history.goBack()
+    }
     return (
         <div>
-            ApplicationForm
+            <h2>ApplicationForm</h2>
+            <button onClick={goToback}>Voltar</button>
         </div>
     )
 }
