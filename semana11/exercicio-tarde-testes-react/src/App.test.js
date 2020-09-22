ce7cbfc1fbc2c5b15fcfac5fc5b664a5583bfcbc
Martin <IchHeibemartus@gmail.com>
2020-09-22 16:34:42 -0300

    Tarefa incompleta, não conseguindo fazer testes

---
+++ semana11/exercicio-tarde-testes-react/src/App.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, fireEvent, getByText, getByRole, getByPlaceholderText, getByDisplayValue } from "@testing-library/react";
+import App from "./App";
+import { Post } from "./components/Post";
+
+const post = () => {
+    return (
+        <Post />
+    )
+}
+
+test("Testando o input que eu irei fracassar segundo o notion", () => {
+    //preparação
+    const { getByRole, getByPlaceholderText } = render(<App />)
+
+    let postInput = getByRole('textbox', {name: /Novo Post/i})
+    fireEvent.change(postInput)
+    expect(postInput).toEqual(true)
+
+    // const botaoAdicionar = getByText(/Adicionar/i)
+
+    //execução
+    // fireEvent.click(botaoAdicionar)
+    // const novoPost = getByText(post)
+    // expect(botaoAdicionar).toContainElement(novoPost)
+    //verificação
+
+})
