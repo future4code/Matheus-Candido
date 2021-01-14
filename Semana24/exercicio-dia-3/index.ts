0baf0425bf22399947b3131cdfd9b39fb4d8210e
Matheus Martins <66175793+Martthus@users.noreply.github.com>
2021-01-13 21:38:47 -0300

    exercicio-dia-3

---
+++ Semana24/exercicio-dia-3/index.ts
@@ -0,0 +1,27 @@
+const parentesesCheck = (str: string): boolean | any => {
+    const stack = []
+
+    for (let char of str) {
+        if (char === "(" || char === "[" || char === "{") {
+            stack.push(char);
+        } else {
+            const lastOpeningChar = stack.pop();
+            if (!lastOpeningChar) {
+                return false
+            } else if (
+                (lastOpeningChar === '(' && char !== ')') ||
+                (lastOpeningChar === "[" && char !== "]") ||
+                (lastOpeningChar === "{" && char !== "}")
+            ) {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length > 0) {
+        return false;
+    }
+
+    return true;
+}
+console.log(parentesesCheck("[}"))
